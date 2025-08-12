f218f12d2699a69a71a7a55c700e5cc381fe19e8
arunthathianu <arunthathianu@gmail.com>
2025-08-12 09:15:15 +0530

    Assgnment

---
+++ MavenProject/src/main/java/collections/ColorsList.java
@@ -0,0 +1,18 @@
+package collections;
+
+import java.util.*;
+
+public class ColorsList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		List<String> color = new ArrayList<String>();
+		color.add("Red");
+		color.add("Blue");
+		color.add("Yellow");
+		System.out.println(color);
+
+	}
+
+}
